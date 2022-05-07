0cda83a79f00cda93c6b85c7392381cc785f7f67
Vladimir Garbuzov <vladimir.garbuzzov@yandex.ru>
2022-05-08 00:35:05 +0300

    write app using options API

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <template>
-  <List />
+  <div class="wrapper">
+    <!-- header -->
+    <div class="wrapper-content">
+      <div class="view">
+        <div class="container">
+          <Form @add-tweet="handleTweet" />
+          <List :items="items"/>
+        </div>
+      </div>
+    </div>
+    <!-- footer -->
+  </div>
 </template>
 
 <script>
+import Form from '@/components/Form'
 import List from '@/components/List'
 export default {
-  name: 'App',
   components: {
+    Form,
     List
+  },
+  data () {
+    return {
+      items: [
+        {
+          id: 1,
+          body: 'hello vue 3',
+          avatar: `https://avatars.dicebear.com/api/male/john.svg`,
+          likes: 12,
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+          id: 2,
+          body: 'hello world',
+          avatar: `https://avatars.dicebear.com/api/female/2.svg`,
+          likes: 8,
+          date: new Date(Date.now()).toLocaleString()
+        }
+      ]
+    }
+  },
+  methods: {
+    handleTweet (item) {
+      this.items.push(item)
+    }
   }
 }
 </script>
